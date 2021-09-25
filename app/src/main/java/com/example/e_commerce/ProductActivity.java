71dd0e45b4731932082db979b185a152635c4223
hemji1912 <hemantvish1999@gmail.com>
2021-09-25 07:30:36 -0700

    final commit

---
+++ app/src/main/java/com/example/e_commerce/ProductActivity.java
@@ -0,0 +1,15 @@
+package com.example.e_commerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product);
+        getSupportActionBar().hide();
+    }
+}
